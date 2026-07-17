409c8c2fc503a025b0ac4a3a933834586b25db3b
lishanthraja-r <lishanthraja12@gmail.com>
2026-07-17 15:09:15 +0530

    Create 89-GrayCode

---
+++ 89-GrayCode/89-GrayCode.java
@@ -0,0 +1,9 @@
+// Last updated: 7/17/2026, 3:09:15 PM
+class Solution {
+    public List<Integer> grayCode(int n) {
+        int size = 1 << n;
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0; i < size; i++) result.add(i ^ (i >> 1));
+        return result;
+    }
+}
