68657e2005c70f93e0d612e6a28e7a6238b4142d
lishanthraja-r <lishanthraja12@gmail.com>
2026-07-17 15:08:16 +0530

    Create 185-DepartmentTopThreeSalaries

---
+++ 185-DepartmentTopThreeSalaries/185-DepartmentTopThreeSalaries.sql
@@ -0,0 +1,7 @@
+-- Last updated: 7/17/2026, 3:08:16 PM
+Select d.name as department , e1.name as employee, e1.salary as Salary
+From Employee e1 join Department d on e1.DepartmentId = d.Id
+Where  3 > (select count(distinct (e2.Salary))
+            from  Employee e2
+            where e2.Salary > e1.Salary
+            and e1.DepartmentId = e2.DepartmentId)
