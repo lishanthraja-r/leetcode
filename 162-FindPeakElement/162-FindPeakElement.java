56914c54f079cf25cf230ea57cd2a616bc2f68a7
lishanthraja-r <lishanthraja12@gmail.com>
2026-07-17 15:08:41 +0530

    Create 162-FindPeakElement

---
+++ 162-FindPeakElement/162-FindPeakElement.java
@@ -0,0 +1,13 @@
+// Last updated: 7/17/2026, 3:08:41 PM
+class Solution {
+    public int findPeakElement(int[] arr) {
+        int n = arr.length;
+        int max = 0;
+        for(int i = 0 ; i<n ; i++){
+            if(arr[i]>arr[max]){
+                max = i;   
+            }
+        }
+        return max;
+    }
+}
