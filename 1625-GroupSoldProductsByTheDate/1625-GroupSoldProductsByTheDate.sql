631f74b14cf9b8068f2d896ec626161bcfee6240
lishanthraja-r <lishanthraja12@gmail.com>
2026-07-17 15:06:41 +0530

    Create 1625-GroupSoldProductsByTheDate

---
+++ 1625-GroupSoldProductsByTheDate/1625-GroupSoldProductsByTheDate.sql
@@ -0,0 +1,6 @@
+-- Last updated: 7/17/2026, 3:06:41 PM
+select sell_date, count( DISTINCT product ) as num_sold ,
+    
+    GROUP_CONCAT( DISTINCT product order by product ASC separator ',' ) as products
+    
+        FROM Activities GROUP BY sell_date order by sell_date ASC;
