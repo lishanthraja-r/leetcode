1cfec13ddd9baa4c39f26c46f3be79dbe1a4c6f6
lishanthraja-r <lishanthraja12@gmail.com>
2026-07-17 15:08:10 +0530

    Create 197-RisingTemperature

---
+++ 197-RisingTemperature/197-RisingTemperature.sql
@@ -0,0 +1,8 @@
+-- Last updated: 7/17/2026, 3:08:10 PM
+SELECT today.id
+FROM Weather yesterday 
+CROSS JOIN Weather today
+
+WHERE DATEDIFF(today.recordDate,yesterday.recordDate) = 1
+    AND today.temperature > yesterday.temperature
+;
